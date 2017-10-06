81617be953a298dc265dacddb1089cc7e955143e
Rahul Anand <rahul1989@gmail.com>
2017-10-06 11:47:57 +0530

    Blueprint for api

--- blockchain.py
+++ blockchain.py
@@ -2,7 +2,8 @@ import hashlib
 import json
 from time import time
 from uuid import uuid4
-
+from textwrap import dedent
+from flask import Flask
 
 class Blockchain(object):
     def __init__(self):
@@ -11,6 +12,38 @@ class Blockchain(object):
 
         #create genesis block
         self.new_block(previous_hash=1,proof=100)
+
+
+    #instantiate node
+    app = Flask(__name__)
+
+    #Generate a globally unique address for this node
+    node_identifier = str(uuid4()).replace('-','')
+
+    #instantiate the blockchain
+    blockchain = Blockchain()
+
+    @app.route('/mine',methods=['GET'])
+
+    def mine():
+        return "We'll mine a new block"
+
+    @app.route('/transactions/new',methods = ['POST'])
+
+    def new_transaction():
+        return "We'll add a new transaction"
+
+    @app.route('/chain',methods=['GET'])
+
+    def full_chain():
+        response = {
+            'chain':blockchain.chain,
+            'length':len(blockchain.chain),
+        }
+        return jsonify(response),200
+
+    if __name__=='__main__':
+        app.run(host='0.0.0.0',port=5000)
 
     def proof_of_work(self,last_proof):
         """
