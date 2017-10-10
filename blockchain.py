6016da4cf3a09c61b5c728aca153bd00fdaa01bd
Rahul Anand <rahul1989@gmail.com>
2017-10-10 12:36:21 +0530

    Implementation of the api

--- blockchain.py
+++ blockchain.py
@@ -3,7 +3,7 @@ import json
 from time import time
 from uuid import uuid4
 from textwrap import dedent
-from flask import Flask
+from flask import Flask, jsonify, request
 
 class Blockchain(object):
     def __init__(self):
@@ -26,12 +26,46 @@ class Blockchain(object):
     @app.route('/mine',methods=['GET'])
 
     def mine():
-        return "We'll mine a new block"
+        #We run the proof of work algorithm to get the next proof
+        last_block = blockchain.last_block
+        last_proof = last_block['proof']
+        proof = blockchain.proof_of_work(last_proof)
+
+        #We must receive a reward for finding the proof
+        #The sender is "0" to signify that this node has mined a new coin
+        blockchain.new_transaction(
+            sender='0',
+            recipient=node_identifier,
+            amount=1,
+        )
+
+        #Forge a new block by adding it to the chain
+        block = blockchain.new_block(proof)
+
+        response = {
+            'message':"New block forged",
+            'index':block['index'],
+            'transactions':block['transactions'],
+            'proof':block['proof']
+            'previous_hash':block['previous_hash'],
+        }
+        return jsonify(response), 200
 
     @app.route('/transactions/new',methods = ['POST'])
 
     def new_transaction():
-        return "We'll add a new transaction"
+        values = request.get_json()
+
+        #Check that teh requested fields are in the POSTed data
+        required = ['sender','recipient','amount']
+        if not all(k in values for k in required):
+            return 'Missing values', 400
+
+        #Create a new transaction
+        index = blockchain.new_transaction(values['sender'],values['recipient'],values['amount'])
+
+        response = {'message':f"Transaction will be added to block {index}"}
+        return jsonify(response),201
 
     @app.route('/chain',methods=['GET'])
 
